0759686f383b8d47b0a084206db4963aace2cf18
David Nize <156432647+davidnize@users.noreply.github.com>
2024-02-01 21:16:01 +0200

    Create function.js

---
+++ function.js
@@ -0,0 +1,63 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+function main(){
+   turnLeft();
+   move();
+   move();
+   move();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+   move();
+   turnRight();
+   move();
+   move();
+   turnRight();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+   move();
+   turnLeft();
+   move();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+   move();
+   turnLeft();
+   move();
+   move();
+   turnRight();
+   turnRight();
+   turnRight();
+   move();
+   turnRight();
+   move();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+   move(); 
+   pickBeeper();  
+}
+
+
+
+
+
+
+
+
