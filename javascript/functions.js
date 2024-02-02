314768f47179abe22c76c0fe7dfc2a807c4cf38c
David Nize <156432647+davidnize@users.noreply.github.com>
2024-02-02 17:24:30 +0200

    Create functions.js

---
+++ javascript/functions.js
@@ -0,0 +1,58 @@
+[Stanford Karel IDE](https://stanford.edu/~cpiech/karel/ide.html)
+
+// thats the link to the the game 
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
+function main(){
+   turnLeft();
+   move();
+   move();
+   move();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+   move();
+   turnRight();
+   move();
+   move();
+   turnRight();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+   move();
+   turnLeft();
+   move();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+   move();
+   turnLeft();
+   move();
+   move();
+   turnRight();
+   turnRight();
+   turnRight();
+   move();
+   turnRight();
+   move();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+   move(); 
+   pickBeeper();  
+}
